ce8a929165b7187eaba1ca60413c9267f6efc3b9
michelle <michelletyson29@gmail.com>
2019-02-01 14:11:51 -0800

    links, bootstrap built cards with forms for the schedule/ new train

---
+++ assets/javascript/train.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+
+  var config = {
+    apiKey: "AIzaSyAlgH9LdQzoVRU8HP22ViqZx_RCz-7MurQ",
+    authDomain: "train-schedule-hw-e720e.firebaseapp.com",
+    databaseURL: "https://train-schedule-hw-e720e.firebaseio.com",
+    projectId: "train-schedule-hw-e720e",
+    storageBucket: "train-schedule-hw-e720e.appspot.com",
+    messagingSenderId: "571375263346"
+  };
+  firebase.initializeApp(config);
+
+
+var database = firebase.database();
+
+//
