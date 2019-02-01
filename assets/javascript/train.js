874dccc181db5efece28137a62bac91d99297353
michelle <michelletyson29@gmail.com>
2019-02-01 14:28:28 -0800

    added ids, pseudocode

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -10,7 +10,27 @@
   };
   firebase.initializeApp(config);
 
-
+//variable to store the database
 var database = firebase.database();
 
-//
+//onclick event when the submit button is clicked
+$("#submit").on("click", function(event){
+    //keep the page from refreshing
+    event.preventDefault();
+    //create variables to store the values of each input from the form 
+
+    //add the stored values to the linked Firebase database 
+
+    //create the table with the information
+
+    // append the table rows to the tbody
+
+    //read a snapshot from the database
+
+    //calculate next arrival and minutes away using moment
+
+    //create another table with these additions when there are new submissions
+    
+
+
+})
